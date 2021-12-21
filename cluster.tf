1c0eca991919009d37b0c00ccd14bfba38a767f9
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-12-20 22:12:17 -0600

    Create cluster.tf

---
+++ cluster.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_cluster" "main" {
+  name     = "${var.name}-${var.environment}"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  vpc_config {
+    subnet_ids = concat(var.public_subnets.*.id, var.private_subnets.*.id)
+  }
+
+  timeouts {
+    delete = "30m"
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.eks_cluster,
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy
+  ]
+}
