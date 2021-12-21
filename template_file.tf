fb7210ac57113ac7ff6473b28a4e5a5eeae952c5
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-12-20 22:19:14 -0600

    Create template_file.tf

---
+++ template_file.tf
@@ -0,0 +1,15 @@
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/templates/kubeconfig.tpl")
+
+  vars = {
+    kubeconfig_name           = "eks_${aws_eks_cluster.main.name}"
+    clustername               = aws_eks_cluster.main.name
+    endpoint                  = data.aws_eks_cluster.cluster.endpoint
+    cluster_auth_base64       = data.aws_eks_cluster.cluster.certificate_authority[0].data
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = data.template_file.kubeconfig.rendered
+  filename = pathexpand("~/.kube/config")
+}
