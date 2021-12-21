ad29f7e9b7e27031139844339854e45566b9cefc
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-12-20 22:50:13 -0600

    Create rbac.tf

---
+++ rbac.tf
@@ -0,0 +1,44 @@
+# Role based access control for k8s admin users 
+resource "kubernetes_cluster_role" "ingress" {
+  metadata {
+    name = "alb-ingress-controller"
+    labels = {
+      "app.kubernetes.io/name"       = "alb-ingress-controller"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+
+  rule {
+    api_groups = ["", "extensions"]
+    resources  = ["configmaps", "endpoints", "events", "ingresses", "ingresses/status", "services"]
+    verbs      = ["create", "get", "list", "update", "watch", "patch"]
+  }
+
+  rule {
+    api_groups = ["", "extensions"]
+    resources  = ["nodes", "pods", "secrets", "services", "namespaces"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "ingress" {
+  metadata {
+    name = "alb-ingress-controller"
+    labels = {
+      "app.kubernetes.io/name"       = "alb-ingress-controller"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+  role_ref {
+    api_group = "rbac.authorization.Kubernetes.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.ingress.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.ingress.metadata[0].name
+    namespace = kubernetes_service_account.ingress.metadata[0].namespace
+  }
+
+  depends_on = [kubernetes_cluster_role.ingress]
+}
